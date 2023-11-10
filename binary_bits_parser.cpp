7dae1bb4bf8025ba12832e107990ff42d3de3a98
alvmiller <134068320+alvmiller@users.noreply.github.com>
2023-11-10 15:00:15 +0200

    Create binary_bits_parser.cpp

---
+++ binary_bits_parser.cpp
@@ -0,0 +1,25 @@
+#include <cassert>
+#include <iostream>
+
+template<typename ... Ts> constexpr unsigned long long 
+binary_bits_parser (unsigned long long res, char symbol, Ts ... symbols) {
+    unsigned digit = (symbol == '1') ? 1 : (symbol == '0') ? 0 : throw "Out of range";
+    if constexpr (sizeof ... (Ts) != 0) {
+        return binary_bits_parser(res * 2 + digit, symbols ...);
+    }
+    return res * 2 + digit;
+}
+
+template<char ... Symbols>
+constexpr unsigned long long operator""_bin()
+{
+  return binary_bits_parser(0ull, Symbols ...);
+}
+
+int main()
+{
+    constexpr unsigned long long x = 10001111_bin;
+    assert(x == 143);
+
+    return 0;
+}
